afae51cb362022fdd6ae072a2808951ce704dfc8
Emanuele <ecerilli1@gmail.com>
2021-09-06 16:25:08 +0200

    start

---
+++ tictactoe.rb
@@ -0,0 +1,23 @@
+
+class Game
+  def initialize(case)
+  @case = case
+  end
+end
+  
+cross = Game.new('X')
+
+
+
+
+
+
+# puts " 
+#        #{one} | #{two} | #{three} 
+#       ---+---+---
+#        #{four} | #{five} | #{six} 
+#       ---+---+---
+#        #{seven} | #{eight} | #{nine} 
+#        "
+
+
